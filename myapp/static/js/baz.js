0a5ee7ab31af763fe948a9996343b414766c03db
jihwang <b3hwang@ucsd.edu>
2017-04-17 22:13:08 -0700

    Added simple html/js that checks whether input string equals baz

---
+++ myapp/static/js/baz.js
@@ -0,0 +1,15 @@
+
+function submitBazForm() {
+  var input = document.getElementById('bazInput').value;
+  var output = document.getElementById('bazResult');
+
+  if(isBaz(input)) {
+    output.innerHTML = 'You entered baz!'
+  } else {
+    output.innerHTML = 'You didn\'t enter baz!'
+  }
+}
+
+function isBaz(text) {
+  return (text === 'baz');
+}
