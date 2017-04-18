cc7d520e4039e175fe5237093d28896c0f11badd
Nathan Ng <n.ng555@gmail.com>
2017-04-17 19:13:48 -0700

    fixed the build (performed local testing this time)

--- myapp/app.js
+++ myapp/app.js
@@ -4,10 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var hello = require('./routes/hello');
+// var static_dir = require('serve-static');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,11 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/hello', hello);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
