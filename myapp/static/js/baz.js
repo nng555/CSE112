65459ea5094be673d3b381cd326c731e870323b3
Thomas Gui <tgui@ucsd.edu>
2017-04-18 16:49:10 -0700

    replaced baz string with constant var

--- myapp/static/js/baz.js
+++ myapp/static/js/baz.js
@@ -1,16 +1,18 @@
+const sTest = "baz";
 function submitBazForm() {
     var input = document.getElementById('bazInput').value;
     var output = document.getElementById('bazResult');
 
     if (isBaz(input)) {
-        output.innerHTML = 'You entered baz!'
+        output.innerHTML = `You entered ${sTest}!`
     } else {
-        output.innerHTML = 'You didn\'t enter baz!'
+        output.innerHTML = `You didn\'t enter ${sTest}!`
     }
 }
 
 function isBaz(text) {
-    return (text === 'baz');
+    return (text === sTest);
 }
 
 module.exports.isBaz = isBaz;
+module.exports.sTest = sTest;
--- myapp/test/baz.js
+++ myapp/test/baz.js
@@ -10,19 +10,19 @@ var assert = require('assert');
 
 /**
  * Tests if the string 'baz' will return true
- * @name bazTrue
+ * @name testTrue
  */
-describe('baz', function() {
+describe('testTrue', function() {
     it('should return true when baz is passed in', function() {
-        assert.equal(true, baz.isBaz('baz'));
+        assert.equal(true, baz.isBaz(baz.sTest));
     });
 });
 
 /**
  * Tests if the string 'foo' will return false
- * @name bazFalse
+ * @name testFalse
  */
-describe('baz', function() {
+describe('testFalse', function() {
     it('should return true when baz is passed in', function() {
         assert.equal(false, baz.isBaz('foo'));
     });
