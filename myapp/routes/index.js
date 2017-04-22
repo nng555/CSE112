63e3b26457226bcc923d079c036bdb1575858106
Alan Xu <ayx002@ucsd.edu>
2017-04-22 14:49:10 -0700

    test mongo setup

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -13,4 +13,47 @@ var router = express.Router();
   res.render('index');
 });
 
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+// var app = express();
+
+mongoose.connect('mongodb://localhost/test');
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true }));
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected");
+});
+
+var Schema = mongoose.Schema;
+var contactSchema = new Schema({
+  Name : String
+});
+
+var contact = mongoose.model('Contact', contactSchema);
+
+router.post('/', function(req, res) {
+  new contact({
+    Name : req.body.name
+  }).save(function (err, data) {
+    if (err) console.log(err);
+    else console.log('Saved : ', data );
+  });
+});
+
+// var temp = new contact({
+//   firstName : 'Alan',
+//   middleName : 'Yian',
+//   lastName : 'Xu'
+// });
+
+// temp.save(function (err, data) {
+//   if (err) console.log(err);
+//   else console.log('Saved : ', data );
+// });
+
+module.exports = contact;
 module.exports = router;
