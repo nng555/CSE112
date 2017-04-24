737e5ffad213e3853553da31fe8dbe1227e83887
Paris Do <pcdo@ucsd.edu>
2017-04-24 14:28:33 -0700

    added promises library

--- myapp/db.js
+++ myapp/db.js
@@ -3,6 +3,7 @@ var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 
 
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
+mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 mongoose.connect('mongodb://admin:shinypigeon3@ds113841.mlab.com:13841/shinypigeon', options);
 
